89c6d9935d36434cd2a261d57e43d79d7293f970
abram <abram@curoverse.com>
2016-01-26 17:58:33 -0500

    PrintEnd function

--- pasta/pasta_interface.go
+++ pasta/pasta_interface.go
@@ -6,6 +6,7 @@ import "bufio"
 type RefVarPrinter interface {
   Header(out *bufio.Writer) error
   Print(vartype int, ref_start, ref_len int, refseq []byte, altseq [][]byte, out *bufio.Writer) error
+  PrintEnd(out *bufio.Writer) error
   Pasta(line string, ref_stream *bufio.Reader, out *bufio.Writer) error
   PastaBegin(out *bufio.Writer) error
   PastaEnd(out *bufio.Writer) error
