2254dfb48381bc2c2630f6791cb47636cfc48401
abram <abram@curoverse.com>
2016-02-16 17:45:16 -0500

    cgivar testing
    * indel-nocall working?

--- pasta/assay/cgivar_test.sh
+++ pasta/assay/cgivar_test.sh
@@ -54,7 +54,6 @@ diff <( ./pasta -action rotini-alt0 -i $odir/$ofn_b.inp ) <( ./pasta -action rot
 diff <( ./pasta -action rotini-alt1 -i $odir/$ofn_b.inp ) <( ./pasta -action rotini-alt1 -i $odir/$ofn_b.out ) || _q "cgivar nocall alt1"
 
 echo ok-nocall
-exit 0
 
 
 ## CGI-VAR with indels and nocalls
@@ -64,34 +63,12 @@ exit 0
 ./pasta -action rstream -param 'p-nocall=0.3:p-indel=0.5:p-indel-nocall=0.8:ref-seed=11223344:seed=1234'  > $odir/cgivar-indel-nocall.inp
 ./pasta -action rotini-cgivar -i $odir/cgivar-indel-nocall.inp | ./pasta -action cgivar-rotini -refstream <( ./pasta -action ref-rstream -param 'ref-seed=11223344:allele=1' ) > $odir/cgivar-indel-nocall.out
 
-diff <( ./pasta -action rotini-ref -i $odir/cgivar-indel-nocall.inp ) <( ./pasta -action rotini-ref -i $odir/cgivar-indel-nocall.out ) || echo "error ref"
-diff <( ./pasta -action rotini-alt0 -i $odir/cgivar-indel-nocall.inp ) <( ./pasta -action rotini-alt0 -i $odir/cgivar-indel-nocall.out ) || echo "error alt0"
-diff <( ./pasta -action rotini-alt1 -i $odir/cgivar-indel-nocall.inp ) <( ./pasta -action rotini-alt1 -i $odir/cgivar-indel-nocall.out ) || echo "error alt1"
+diff <( ./pasta -action rotini-ref -i $odir/cgivar-indel-nocall.inp ) \
+  <( ./pasta -action rotini-ref -i $odir/cgivar-indel-nocall.out ) || _q "error indel-nocall ref"
+diff <( ./pasta -action rotini-alt0 -i $odir/cgivar-indel-nocall.inp ) \
+  <( ./pasta -action rotini-alt0 -i $odir/cgivar-indel-nocall.out ) || _q "error indel-nocall alt0"
+diff <( ./pasta -action rotini-alt1 -i $odir/cgivar-indel-nocall.inp ) \
+  <( ./pasta -action rotini-alt1 -i $odir/cgivar-indel-nocall.out ) || _q "error indel-nocall alt1"
 
 echo ok-indel-nocall
-
-
-exit 0
-
-#diff $odir/gvcf-nocall.inp $odir/gvcf-nocall.out
-#diff <( cat $odir/gvcf-nocall.inp | tr -d '\n' | fold -w 50 ) <( cat $odir/gvcf-nocall.out | tr -d '\n'  | fold -w 50 )
-diff <( cat $odir/gvcf-nocall.inp | tr -d '\n' | sed 's/[ACTG]*$//' | fold -w 50 ) <( cat $odir/gvcf-nocall.out | tr -d '\n' | sed 's/[ACTG]*$//' | fold -w 50 )
-
-
-## CGI-VAR with het nocall
-##
-refseed="11223344"
-altseed="1234"
-
-param_inp="p-indel-nocall=0.5:p-indel=0.5:ref-seed=$refseed:seed=$altseed:p-nocall=0.3"
-param_ref="ref-seed=$refseed:allele=1"
-
-./pasta -action rstream -param "$param_inp" > $odir/gvcf-indel-nocall.inp
-./pasta -action rotini-gvcf -i $odir/gvcf-indel-nocall.inp | ./pasta -action gvcf-rotini -refstream <( ./pasta -action ref-rstream -param "$param_ref" ) > $odir/gvcf-indel-nocall.out
-
-diff <( ./pasta -action rotini-ref -i $odir/gvcf-indel-nocall.inp ) <( ./pasta -action rotini-ref -i $odir/gvcf-indel-nocall.out )
-diff <( ./pasta -action rotini-alt0 -i $odir/gvcf-indel-nocall.inp ) <( ./pasta -action rotini-alt0 -i $odir/gvcf-indel-nocall.out )
-diff <( ./pasta -action rotini-alt1 -i $odir/gvcf-indel-nocall.inp ) <( ./pasta -action rotini-alt1 -i $odir/gvcf-indel-nocall.out )
-
-echo ok
 exit 0
